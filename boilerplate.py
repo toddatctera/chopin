b9ad43b1585c919f6158e646178b0b60ea9fb0a3
Todd Butters <todd@ctera.com>
2020-05-01 00:01:09 -0400

    Added logging.

--- boilerplate.py
+++ boilerplate.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
+# to save logs, run `export CTERASDK_LOG_FILE=<logname>`
 from cterasdk import *
 import getpass
 import os.path
+import logging
 
 filename = os.path.basename(__file__)
 
-print("This script is a simple starting point.")
+config.Logging.get().enable()
+config.Logging.get().setLevel(logging.INFO)
+
 # Read input from keyboard and store into global variables.
-portal = input("Please enter the portal address: ")
-username = input("Please enter the global admin username: ")
-password = getpass.getpass("Please enter the password for " + username + ": ")
-domain = input("Please enter the domain: ")
+try:
+    logging.info("Starting script: " + filename)
+    portal = input("Please enter the portal address: ")
+    username = input("Please enter the global admin username: ")
+    password = getpass.getpass("Please enter the password for " + username + ": ")
+    domain = input("Please enter the domain: ")
+except CTERAException as error:
+    logging.error(error)
 
 if __name__ == "__main__":
     try:
         admin = GlobalAdmin(portal)
-        print("Logging into " + portal)
+        logging.info("Logging into " + portal)
         admin.login(username, password)
-        print("Successfully logged in to " + portal)
+        logging.info("Successfully logged in to " + portal)
     except CTERAException as error:
-        print(error)
+        logging.warning(error)
     admin.logout()
-    print("Logged out of " + portal)
-    print('Exiting ' + filename + '.\nCheers.')
+    logging.info("Logged out of " + portal)
+    logging.info('Exiting script: ' + filename)
--- create_cloud_drive_folder.py
+++ create_cloud_drive_folder.py
@@ -4,6 +4,7 @@ from cterasdk import *
 import getpass
 import os.path
 import time
+import logging
 
 filename = os.path.basename(__file__)
 portal = None
@@ -15,19 +16,22 @@ folder_group = 'portal-CloudFolders'
 timestr = time.strftime("%Y-%m-%d-%H-%M-%S")
 file_failed_users = 'failed-users-' + timestr + '.txt'
 #file_failed_users = 'failed-users.txt'
+config.Logging.get().enable()
+config.Logging.get().setLevel(logging.INFO)
 
-print("This script creats a new cloud folder for all valid domain users.")
+logging.info('Starting script: ' + filename)
 
 if __name__ == "__main__":
     try:
+        print("This script creats a new cloud folder for all valid domain users.")
         portal = input("Please enter the portal address: ") 
         username = input("Please enter the global admin username: ") 
         admin = GlobalAdmin(portal)
         password = getpass.getpass("Please enter the password for " + username + ": ") 
-        print("Logging into " + portal)
+        logging.info('Logging into ' + portal)
         admin.login(username, password)
-        print("Successfully logged in to " + portal)
-        domain = input("Please enter the domain: ") 
+        logging.info('Successfully logged in to ' + portal)
+        domain = input('Please enter the domain: ') 
         cloud_folder = input("Please enter a name for the new Cloud Folder (Default='My Files'): ") or 'My Files'
         print("Printing Possible Folder Group Names to hold the new Cloud Folder...")
         print("\t" + "Folder Group Names")
@@ -37,24 +41,21 @@ if __name__ == "__main__":
         folder_group = input("Please enter one of the Folder Groups printed above to use (Default='portal-CloudFolders'):\n>") or 'portal-CloudFolders'
         users = admin.users.list_domain_users(domain)
         failed_users = []
-        print("Creating domain users array.")
-        print("Looping through array...")
         for user in users:
-            print("Username is " + user.name)
             user_account = portal_types.UserAccount(user.name,domain)
-            print("User account is " + user_account.name) 
-            print("Creating " + cloud_folder + ' for ' + user.name)
             try:
                 admin.cloudfs.mkdir(cloud_folder, folder_group, user_account)
+                logging.info('Created Cloud Folder, ' + cloud_folder + ', for ' + user.name)
             except CTERAException as error:
-                print(error)
-                print("Failed to make cloud folder for " + user.name)
+                logging.error(error)
+                logging.warning('Failed to make Cloud Folder, ' + cloud_folder + ', for ' + user.name)
                 failed_users.append(user.name)
     except CTERAException as error:
         print(error)
-    print("Failed to make cloud folder for the following users:")
-    for user in failed_users:
-        print(user)
+    if failed_users:
+        print('Failed to make cloud folder for the following users:')
+        for user in failed_users:
+            print(user)
 
     # Write failed users to an output file
     failed_file = open(file_failed_users,'w') 
@@ -62,5 +63,5 @@ if __name__ == "__main__":
         failed_file.write(user + '\n')
     failed_file.close()    
     admin.logout()
-    print("Logged out of " + portal)
-    print('Exiting ' + filename + '.\nCheers.')
+    logging.info("Logged out of " + portal)
+    logging.info('Exiting script' + filename)
