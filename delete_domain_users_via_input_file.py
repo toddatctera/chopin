d8f9b114446efd1d4495e6455d3d779963ebee17
Todd Butters <todd@ctera.com>
2020-06-05 16:31:46 -0400

    [WIP] initial commit of untested script. Need to test on another system :)

---
+++ delete_domain_users_via_input_file.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+import os.path
+import logging
+import getpass
+from cterasdk import *
+
+# Initialize global variables
+portal = None
+username = None
+password = None
+domain = None
+input_file = None
+accounts = []
+script_name = os.path.basename(__file__)
+# Specify hardcoded input file name. Change to your liking.
+input_file = 'delete-users.txt'
+
+# Enable and set logging level.
+config.Logging.get().enable()
+config.Logging.get().setLevel(logging.INFO)
+
+# Log script start and prompt to set variables
+logging.info('Starting script: ' + script_name)
+print("This script deletes domain users from the portal " + 
+        "based on an input file.")
+portal = input("Enter the portal address: ")
+username = input("Enter the global admin username: ")
+password = getpass.getpass("Enter the password for " + username + ": ")
+domain = input("Enter the domain: ")
+
+if __name__ == "__main__":
+    try:
+        # Log into portal.
+        admin = GlobalAdmin(portal)
+        print("Logging into " + portal)
+        admin.login(username, password)
+        # Read usernames from input file into accounts list.
+        with open(input_file) as users:
+            for user in users:
+                try:
+                    account = portal_types.UserAccount(user.rstrip('\n'), domain)
+                    accounts.append(account)
+                except CTERAException as error:
+                    logging.error(error)
+        # Loop through accounts and try to delete them.
+        for account in accounts:
+            try:
+                admin.users.delete(account.name)
+                logging.info('Deleted user account: ' + account.name)
+            except CTERAException as error:
+                logging.error(error)
+                logging.info('Probably failed to delete account: ' + account.name)
+    except CTERAException as error:
+        logging.error(error)
+    admin.logout()
