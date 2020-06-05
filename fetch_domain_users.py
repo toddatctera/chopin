9ca0c52de008f0f6309045bf2a781df5cba878a1
Todd Butters <todd@ctera.com>
2020-06-05 17:05:29 -0400

    Updated script description and removed a commented line.

--- fetch_domain_users.py
+++ fetch_domain_users.py
@@ -2,7 +2,7 @@
 from cterasdk import *
 import getpass
 
-print("This script tries to fetch hardcoded domain users or groups")
+print("This script fetches the hardcoded domain users group")
 portal = input("Please enter the portal address: ")
 username = input("Please enter the global admin username: ")
 password = getpass.getpass("Please enter the password for " + username + ": ")
@@ -14,7 +14,6 @@ if __name__ == "__main__":
         print("Logging into " + portal)
         admin.login(username, password)
         print("Creating user/group objects to be fetched...") 
-        #user = portal_types.UserAccount('todd',domain)
         group = portal_types.GroupAccount('Domain Users',domain)
         print("Fetching user accounts and groups...")
         admin.directoryservice.fetch([group])
