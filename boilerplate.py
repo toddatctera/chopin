84ebb787d65e7afd7d01fd1b0ec60afc757f113e
Todd Butters <todd@ctera.com>
2020-11-02 15:00:08 -0500

    Moved test code into its own function named run()

--- boilerplate.py
+++ boilerplate.py
@@ -11,23 +11,26 @@ config.Logging.get().enable()
 config.Logging.get().setLevel(logging.INFO)
 
 # Read input from keyboard and store into global variables.
-try:
-    logging.info("Starting script: " + filename)
-    portal = input("Please enter the portal address: ")
-    username = input("Please enter the global admin username: ")
-    password = getpass.getpass("Please enter the password for " + username + ": ")
-    domain = input("Please enter the domain: ")
-except CTERAException as error:
-    logging.error(error)
-
-if __name__ == "__main__":
+def run():
     try:
-        admin = GlobalAdmin(portal)
-        logging.info("Logging into " + portal)
-        admin.login(username, password)
-        logging.info("Successfully logged in to " + portal)
+        logging.info("Starting script: " + filename)
+        portal = input("Please enter the portal address: ")
+        username = input("Please enter the global admin username: ")
+        password = getpass.getpass("Please enter the password for " + username + ": ")
+        domain = input("Please enter the domain: ")
     except CTERAException as error:
-        logging.warning(error)
-    admin.logout()
-    logging.info("Logged out of " + portal)
-    logging.info('Exiting script: ' + filename)
+        logging.error(error)
+
+    if __name__ == "__main__":
+        try:
+            admin = GlobalAdmin(portal)
+            logging.info("Logging into " + portal)
+            admin.login(username, password)
+            logging.info("Successfully logged in to " + portal)
+        except CTERAException as error:
+            logging.warning(error)
+        admin.logout()
+        logging.info("Logged out of " + portal)
+        logging.info('Exiting script: ' + filename)
+
+run()
